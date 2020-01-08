84f8ab067e107b4b25be48a303ddab15d654a792
jay51 <neneojaafar3@gmail.com>
2020-01-07 22:51:11 -0600

    uploaded my post from dev to and add gh links on home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,12 @@ module.exports = {
     description: `This blog will discuss various concept, methods and technologies that interest the author such as operating systems, Linux, reverse engineering, ect..`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        name: `Linkedin`,
+        url: `https://www.linkedin.com/in/jamal-al-119a14152/`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/jay51`,
       },
     ],
   },
